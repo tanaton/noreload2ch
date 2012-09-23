732f936a29e1c06ad693055f38603cbec3bd2e08
tanaton <heiwaboke@gmail.com>
2012-09-24 03:03:28 +0900

    ’’

--- noreload.go
+++ noreload.go
@@ -4,6 +4,7 @@ package main
 import (
 	"bytes"
 	"encoding/json"
+	"errors"
 	"fmt"
 	"io/ioutil"
 	"log"
@@ -12,6 +13,7 @@ import (
 	"regexp"
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 	"code.google.com/p/go.net/websocket"
 	"github.com/tanaton/get2ch-go"
@@ -31,34 +33,38 @@ const (
 )
 
 type Nich struct {
-	server		string
-	board		string
-	thread		string
+	server			string
+	board			string
+	thread			string
 }
 
 type Board struct {
-	n			Nich
-	res			int
-	speed		float64
+	n				Nich
+	res				int
+	speed			float64
 }
 
-type WsServer struct {
-	sh			string
-	sp			int
-	board		string
+type DispatchHandler struct {
+	mwh				map[string]websocket.Handler
+	mwhu			sync.RWMutex
+	logger			*log.Logger
+	slch			<-chan *map[string]string
 }
 
 type WsConnection struct {
-	listen		map[*websocket.Conn]chan error
-	logger		*log.Logger
-	s			*WsServer
+	listen			map[*websocket.Conn]chan error
+	mux				sync.RWMutex
+	board			string
+	dh				*DispatchHandler
 }
 
 type Config struct {
-	v			map[string]interface{}
-	wh			string
-	wp			int
-	wl			[]*WsServer
+	v				map[string]interface{}
+	wh				string
+	wp				int
+	ReadTimeoutSec	int
+	WriteTimeoutSec	int
+	MaxHeaderBytes	int
 }
 
 var g_reg_bbs *regexp.Regexp = regexp.MustCompile("(.+\\.2ch\\.net|.+\\.bbspink\\.com)/(.+)<>")
@@ -69,51 +75,96 @@ func main() {
 	c := readConfig()
 	slch := getServerCh()
 	logger := log.New(os.Stdout, "", log.Ldate | log.Ltime | log.Lmicroseconds)
-	for _, it := range c.wl {
-		http.Handle("/" + it.board, websocket.Handler(createWebHandler(it, logger, slch)))
+	myHandler := &DispatchHandler{
+		mwh		: make(map[string]websocket.Handler),
+		logger	: logger,
+		slch	: slch,
 	}
-	logger.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%d", c.wh, c.wp), nil))
+	server := &http.Server{
+		Addr			: fmt.Sprintf("%s:%d", c.wh, c.wp),
+		Handler			: myHandler,
+		ReadTimeout		: time.Duration(c.ReadTimeoutSec) * time.Second,
+		WriteTimeout	: time.Duration(c.WriteTimeoutSec) * time.Second,
+		MaxHeaderBytes	: c.MaxHeaderBytes,
+	}
+	// サーバ起動
+	logger.Fatal(server.ListenAndServe())
 }
 
-func createWebHandler(wss *WsServer, lg *log.Logger, slch <-chan *map[string]string) func(ws *websocket.Conn) {
+func (dh *DispatchHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	b, err := dh.checkPath(r.URL.Path)
+	if err == nil {
+		dh.match(b).ServeHTTP(w, r)
+	} else {
+		w.WriteHeader(http.StatusBadRequest)
+	}
+}
+
+func (dh *DispatchHandler) checkPath(path string) (string, error) {
+	l := strings.Split(path, "/")
+	if len(l) != 2 { return "", errors.New("path error") }
+	sl := <-dh.slch
+	if _, ok := (*sl)[l[1]]; !ok { return "", errors.New("path error") }
+	return l[1], nil
+}
+
+func (dh *DispatchHandler) match(b string) websocket.Handler {
+	dh.mwhu.RLock()
+	defer dh.mwhu.RUnlock()
+	wh, ok := dh.mwh[b]
+	if !ok {
+		wh = websocket.Handler(createWebHandler(b, dh))
+		dh.mwh[b] = wh
+	}
+	return wh
+}
+
+func createWebHandler(board string, dh *DispatchHandler) func(ws *websocket.Conn) {
 	wsc := &WsConnection{
 		listen	: make(map[*websocket.Conn]chan error),
-		logger	: lg,
-		s		: wss,
+		board	: board,
+		dh		: dh,
 	}
-	go wsc.timeCallback(slch)
+	go wsc.timeCallback()
 
 	return func(ws *websocket.Conn) {
 		ech := make(chan error, 1)
 		defer ws.Close()
 		defer close(ech)
-		wsc.logger.Printf("connect!")
+		wsc.dh.logger.Printf("connect!")
+
+		wsc.mux.Lock()
 		wsc.listen[ws] = ech
+		wsc.mux.Unlock()
+
 		err := <-ech
 		if err != nil {
-			wsc.logger.Printf("%s", err.Error())
+			wsc.dh.logger.Printf("%s", err.Error())
 		}
+
+		wsc.mux.Lock()
 		delete(wsc.listen, ws)
+		wsc.mux.Unlock()
 	}
 }
 
-func (wsc *WsConnection) timeCallback(slch <-chan *map[string]string) {
+func (wsc *WsConnection) timeCallback() {
 	// 同時送信数の制限
 	sync := make(chan bool, MAX_PROCS)
-	defer close(sync)
+	defer wsc.dispose(sync)
 	// 一定期間で発火するインターバルタイマの生成
 	tick := time.Tick(IGNITION_TIME)
-	nch, fire := getBoardCh(wsc.s, slch)
+	nch, fire := getBoardCh(wsc.board, wsc.dh.slch)
 
 	for _ = range tick {
-		if len(wsc.listen) == 0 {
-			// 接続がない時はスキップ
-			continue
+		if wsc.isConnection() == false {
+			// 接続がない時は終了する
+			break
 		}
 		n := <-nch
-		data, resno, err := getData(n, wsc.s)
+		data, resno, err := getData(n)
 		if err != nil {
-			wsc.logger.Printf("Error:%s", err.Error())
+			wsc.dh.logger.Printf("Error:%s", err.Error())
 			continue
 		} else if data == nil {
 			// 更新無し
@@ -123,15 +174,30 @@ func (wsc *WsConnection) timeCallback(slch <-chan *map[string]string) {
 			<-nch
 		}
 		dl := time.Now().Add(WS_DEADLINE_TIME)
+		wsc.mux.RLock()
 		for socket, ech := range wsc.listen {
-			sync <- true
 			socket.SetDeadline(dl)
-			go wsc.writeData(data, socket, ech, sync)
+			sync <- true
+			go writeData(data, socket, ech, sync)
 		}
+		wsc.mux.RUnlock()
 	}
 }
 
-func (wsc *WsConnection) writeData(data []byte, con *websocket.Conn, ech chan error, sync <-chan bool) {
+func (wsc *WsConnection) isConnection() bool {
+	wsc.mux.RLock()
+	defer wsc.mux.RUnlock()
+	return len(wsc.listen) != 0
+}
+
+func (wsc *WsConnection) dispose(sync chan bool) {
+	wsc.dh.mwhu.Lock()
+	defer wsc.dh.mwhu.Unlock()
+	delete(wsc.dh.mwh, wsc.board)
+	close(sync)
+}
+
+func writeData(data []byte, con *websocket.Conn, ech chan error, sync <-chan bool) {
 	_, werr := con.Write(data)
 	if werr != nil {
 		ech <- werr
@@ -139,11 +205,11 @@ func (wsc *WsConnection) writeData(data []byte, con *websocket.Conn, ech chan er
 	<-sync
 }
 
-func getData(n Nich, wss *WsServer) ([]byte, int, error) {
+func getData(n Nich) ([]byte, int, error) {
 	var str, title string
 	var resno int
 	get := get2ch.NewGet2ch(get2ch.NewFileCache(DAT_DIR))
-	if wss.sh != "" { get.SetSalami(wss.sh, wss.sp) }
+	//if wss.sh != "" { get.SetSalami(wss.sh, wss.sp) }
 	moto, err := get.Cache.GetData(n.server, n.board, n.thread)
 	if err != nil { moto = nil }
 	err = get.SetRequest(n.server, n.board, n.thread)
@@ -214,15 +280,15 @@ func getServer() map[string]string {
 	return sl
 }
 
-func getBoardCh(wss *WsServer, slch <-chan *map[string]string) (<-chan Nich, chan<- bool) {
+func getBoardCh(board string, slch <-chan *map[string]string) (<-chan Nich, chan<- bool) {
 	ch := make(chan Nich)
 	fire := make(chan bool, 1)
 	go func() {
 		var nich Nich
 		f := func() {
 			sl := <-slch
-			if s, ok := (*sl)[wss.board]; ok {
-				nich = getBoard(s, wss)
+			if s, ok := (*sl)[board]; ok {
+				nich = getBoard(s, board)
 			}
 		}
 		tch := time.Tick(BOARD_CYCLE_TIME)
@@ -241,11 +307,12 @@ func getBoardCh(wss *WsServer, slch <-chan *map[string]string) (<-chan Nich, cha
 	return ch, fire
 }
 
-func getBoard(s string, wss *WsServer) (n Nich) {
+func getBoard(s, board string) (n Nich) {
 	now := float64(time.Now().Unix())
 	get := get2ch.NewGet2ch(get2ch.NewFileCache(DAT_DIR))
-	if wss.sh != "" { get.SetSalami(wss.sh, wss.sp) }
-	err := get.SetRequest(s, wss.board, "")
+//	if wss.sh != "" { get.SetSalami(wss.sh, wss.sp) }
+//	err := get.SetRequest(s, wss.board, "")
+	err := get.SetRequest(s, board, "")
 	if err != nil { return }
 	data, err := get.GetData()
 	if err != nil {
@@ -257,7 +324,7 @@ func getBoard(s string, wss *WsServer) (n Nich) {
 	for _, it := range list {
 		if d := g_reg_dat.FindStringSubmatch(it); len(d) == 3 {
 			n.server = s
-			n.board = wss.board
+			n.board = board
 			n.thread = d[1]
 			num, e1 := strconv.Atoi(d[1])
 			res, e2 := strconv.Atoi(d[2])
@@ -349,21 +416,9 @@ func (c *Config) read(filename string) {
 
 	c.wh = c.getDataString("WSHost", "localhost")
 	c.wp = c.getDataInt("WSPort", 8000)
-	c.wl = make([]*WsServer, 0, 1)
-	l := c.getDataArray("List", nil)
-	for _, it := range l {
-		if data, ok := it.(map[string]interface{}); ok {
-			subc := &Config{v: data}
-			wss := &WsServer{
-				sh		: subc.getDataString("SalamiHost", ""),
-				sp		: subc.getDataInt("SalamiPort", 80),
-				board	: subc.getDataString("Board", ""),
-			}
-			if wss.board != "" {
-				c.wl = append(c.wl, wss)
-			}
-		}
-	}
+	c.ReadTimeoutSec = c.getDataInt("ReadTimeoutSec", 10)
+	c.WriteTimeoutSec = c.getDataInt("WriteTimeoutSec", 10)
+	c.MaxHeaderBytes = c.getDataInt("MaxHeaderBytes", 1024 * 10)
 }
 
 func (c *Config) getDataInt(h string, def int) (ret int) {
@@ -381,34 +436,6 @@ func (c *Config) getDataString(h, def string) (ret string) {
 	if it, ok := c.v[h]; ok {
 		if ret, ok = it.(string); !ok {
 			ret = def
-		}
-	}
-	return
-}
-
-func (c *Config) getDataArray(h string, def []interface{}) (ret []interface{}) {
-	ret = def
-	if it, ok := c.v[h]; ok {
-		if arr, ok := it.([]interface{}); ok {
-			ret = arr
-		}
-	}
-	return
-}
-
-func (c *Config) getDataStringArray(h string, def []string) (ret []string) {
-	sil := c.getDataArray(h, nil)
-
-	ret = def
-	if sil != nil {
-		ret = make([]string, 0, 1)
-		for _, it := range sil {
-			if s, ok := it.(string); ok {
-				ret = append(ret, s)
-			} else {
-				ret = def
-				break
-			}
 		}
 	}
 	return
